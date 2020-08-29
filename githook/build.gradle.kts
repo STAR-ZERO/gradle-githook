c6b7cfd565a4a47bc244f032012948436b15d3f1
Kenji Abe <kenji.01.star@gmail.com>
2020-08-29 18:15:47 +0900

    Update publish plugin

--- githook/build.gradle.kts
+++ githook/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `kotlin-dsl`
     `maven-publish`
     id("org.jlleitschuh.gradle.ktlint") version "4.1.0"
-    id("com.gradle.plugin-publish") version "0.9.10"
+    id("com.gradle.plugin-publish") version "0.12.0"
 }
 
 group = "com.star-zero.gradle"
